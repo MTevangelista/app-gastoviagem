02bdfd94bfe9d00567c5e6b40b88a8e80d72091c
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-11-25 18:07:24 -0300

    add function to calculate trip expenses

--- app/src/main/java/com/example/gastodeviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastodeviagem/MainActivity.kt
@@ -2,15 +2,46 @@ package com.example.gastodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val test: String = "Matheus"
-        println(test)
+        
+        setListeners()
     }
-
-
+    
+    private fun setListeners() {
+        buttonCalculate.setOnClickListener(this)
+    }
+    
+    override fun onClick(view: View) {
+        val id = view.id
+        
+        if (id == R.id.buttonCalculate) {
+            calculate()
+        }
+    }
+    
+    private fun calculate() {
+        if (validationOk()) {
+            val distance = editTxtDistance.text.toString().toFloat()
+            val price = editTxtPrice.text.toString().toFloat()
+            val autonomy = editTxtAutonomy.text.toString().toFloat()
+            
+            val totalValue = (distance * price) / autonomy
+            txtViewTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        }
+    }
+    
+    private fun validationOk(): Boolean {
+        return (
+                editTxtDistance.text.toString() != ""
+                && editTxtPrice.text.toString() != ""
+                && editTxtAutonomy.text.toString() != ""
+        )
+    }
+    
 }
